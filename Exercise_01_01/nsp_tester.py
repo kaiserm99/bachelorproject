8dd60bd51cadda526c40455a4e0fd3885ce484c6
kaiserm99 <kaiserm@informatik.uni-freiburg.de>
2020-10-06 23:16:07 +0200

    NSP still not working, added the current try.

--- Exercise_01_01/nsp_tester.py
+++ Exercise_01_01/nsp_tester.py
@@ -1,6 +1,11 @@
-lst = ['fridafo', 'fridaft', 'fridano', 'fridant', 'fridaso', 'fridast', 'fritzfo', 'fritzft', 'fritzno', 'fritzso', 'fritzst', 'heinzfo', 'heinzft', 'heinzno', 'heinznt', 'heinzso', 'heinzst', 'irafo', 'iraft', 'irano', 'irant', 'iraso', 'irast', 'norafo', 'noraft', 'norano', 'norant', 'noraso', 'norast', 'udofo', 'udoft', 'udono', 'udont', 'udoso', 'udost']
+lst = ['fridafo', 'fridaft', 'fridano', 'fridant', 'fridaso', 'fridast', 'fritzfo', 
+       'fritzft', 'fritzno', 'fritznt', 'fritzso', 'fritzst', 'heinzfo', 'heinzft',
+       'heinzno', 'heinznt', 'heinzso', 'heinzst', 'irafo', 'iraft', 'irano', 'irant',
+       'iraso', 'irast', 'norafo', 'noraft', 'norano', 'norant', 'noraso', 'norast',
+       'udofo', 'udoft', 'udono', 'udont', 'udoso', 'udost']
 
-z = [-1, -2, -3, -4, 5, 6, -7, -8, -9, 10, 11, -12, 13, -14, -15, -16, -17, -18, 19, -20, -21, 22, -23, -24, -25, 26, 27, -28, -29, -30, -31, 32, 33, -34, -35]
+
+z = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17, -18, -19, -20, -21, -22, -23, -24, -25, -26, -27, -28, -29, -30, -31, -32, -33, -34, -35, -36]
 
 
 for n, a in enumerate(lst):
