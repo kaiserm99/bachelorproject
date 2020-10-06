18598f58c361634eacdfda2c8368c428046f4cb0
kaiserm99 <kaiserm@informatik.uni-freiburg.de>
2020-10-06 22:20:37 +0200

    Sicherung

--- Exercise_01_01/all_modells.sh
+++ Exercise_01_01/all_modells.sh
@@ -1,24 +1,39 @@
 #!/bin/sh
+
 SOLUTIONS=0
 cp $1 /tmp/tmpsat
+rm -f $2  # If there is already a Output file, just remove it to update it
 
 while :; do
 
-  minisat -verb=0 /tmp/tmpsat /tmp/tmpout
+	minisat -verb=0 /tmp/tmpsat /tmp/tmpout
 
-  if [ `head -1 /tmp/tmpout` = UNSAT ]; then
-    break
-  fi
+	if [ `head -1 /tmp/tmpout` = UNSAT ]; then  # Check if the calculated output contains UNSAT
+		
+		if [ $SOLUTIONS -eq 0 ]; then  # If there was no solution yet, then the file should contain UNSAT
+	    	echo "UNSAT" > $2  
+		fi
+		
+		break
+	fi
 
-  SOLUTIONS=$((SOLUTIONS + 1))
+	if [ $SOLUTIONS -eq 0 ]; then  # If there is minimum one solution, then write SAT on top of the Output file
+		echo "SAT" > $2
+	fi
 
-  tail -1 /tmp/tmpout |
-    awk '{
-      for(i=1;i<NF;++i) { $i = -$i }
-      print
-    }' >> /tmp/tmpsat
+	SOLUTIONS=$((SOLUTIONS + 1))
 
-  perl -pe  's/(\d+)$/$1+1/e' /tmp/tmpsat
+	tail -1 /tmp/tmpout >> $2  # Write the calculated solution into the Output file
+
+	tail -1 /tmp/tmpout |
+		awk '{
+	    	for(i=1;i<NF;++i) { $i = -$i }
+	    	print
+	   	}' >> /tmp/tmpsat  # Negate the whole output and append it to the file, which is minisat'ed next
+
+	perl -pe 's/(\d+)$/$1+1/e if $. == 1' /tmp/tmpsat > /tmp/tmpfile  # Increase the variable count
+
+	cp /tmp/tmpfile /tmp/tmpsat
 
 done
 
@@ -26,3 +41,4 @@ echo There are $SOLUTIONS solutions.
 
 rm -f /tmp/tmpsat
 rm -f /tmp/tmpout
+rm -f /tmp/tmpfile
