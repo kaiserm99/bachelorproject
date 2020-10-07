451159a1a6441481dbb2dc35c9fd7a7202a60a10
kaiserm99 <kaiserm@informatik.uni-freiburg.de>
2020-10-07 23:27:46 +0200

    Deleted Tabs and added spaces instead.

--- sheet02/Exercise_01/exercise_02_01_a.py
+++ sheet02/Exercise_01/exercise_02_01_a.py
@@ -2,32 +2,32 @@ from constraint import *
 
 
 def main():
-	problem = Problem()
+    problem = Problem()
 
-	domain = [0, 1, 2]
-	variables = ["WA", "NT", "Q", "NSW", "V", "SA", "T"]
+    domain = [0, 1, 2]
+    variables = ["WA", "NT", "Q", "NSW", "V", "SA", "T"]
 
-	problem.addVariables(variables, domain)
+    problem.addVariables(variables, domain)
 
-	# All constraints
-	problem.addConstraint(lambda x, y: x != y, ("WA", "NT"))
-	problem.addConstraint(lambda x, y: x != y, ("WA", "SA"))
-	problem.addConstraint(lambda x, y: x != y, ("NT", "SA"))
-	problem.addConstraint(lambda x, y: x != y, ("NT", "Q"))
-	problem.addConstraint(lambda x, y: x != y, ("SA", "Q"))
-	problem.addConstraint(lambda x, y: x != y, ("SA", "NSW"))
-	problem.addConstraint(lambda x, y: x != y, ("SA", "V"))
-	problem.addConstraint(lambda x, y: x != y, ("Q", "NSW"))
-	problem.addConstraint(lambda x, y: x != y, ("V", "NSW"))
+    # All constraints
+    problem.addConstraint(lambda x, y: x != y, ("WA", "NT"))
+    problem.addConstraint(lambda x, y: x != y, ("WA", "SA"))
+    problem.addConstraint(lambda x, y: x != y, ("NT", "SA"))
+    problem.addConstraint(lambda x, y: x != y, ("NT", "Q"))
+    problem.addConstraint(lambda x, y: x != y, ("SA", "Q"))
+    problem.addConstraint(lambda x, y: x != y, ("SA", "NSW"))
+    problem.addConstraint(lambda x, y: x != y, ("SA", "V"))
+    problem.addConstraint(lambda x, y: x != y, ("Q", "NSW"))
+    problem.addConstraint(lambda x, y: x != y, ("V", "NSW"))
 
 
-	for n, sol in enumerate(problem.getSolutions()):
-		print(("%d. Solution:") % (n+1))
-		print(sol, end="\n\n")
+    for n, sol in enumerate(problem.getSolutions()):
+        print(("%d. Solution:") % (n+1))
+        print(sol, end="\n\n")
 
 
 if __name__ == '__main__':
-	main()
+    main()
 
 
 
